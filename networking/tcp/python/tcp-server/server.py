b72f3bb59b358bf54ca9c90db14bc67e7ddbde7e
svmod3 <severemod3@gmail.com>
2026-08-06 15:24:52 +0200

    refactor(tcp-server): improve server structure and error handling

--- networking/tcp/python/tcp-server/server.py
+++ networking/tcp/python/tcp-server/server.py
@@ -1,16 +1,54 @@
 import socket
 
-HOST = 'localhost'
+HOST = '127.0.0.1'
 PORT = 30001
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
+
+
+def start_server(host, port):
+
+    try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+
+                server.bind((host, port))
+                server.listen(1)
+                server.settimeout(1)
+                print(f"Server listening on {host}:{port}")
+
+                while True:
+                    try:
+                        conn, addr = server.accept()
+                        handle_client(conn, addr)
+                    except socket.timeout:
+                        continue
+
+    except KeyboardInterrupt:
+        print("Server stopped...")
+
+    except OSError as e:
+        print(f"Socket Error: {e}")
+
+    except Exception as e:
+        print(f"Server error: {e}")
+
+
+
+def handle_client(conn, addr):
+
 
     with conn:
         print('Connected by:', addr)
+
         while True:
             data = conn.recv(1024)
-            if not data: break;
+
+            if not data:
+                break
+
             conn.sendall(data)
+
+
+
+
+if __name__ == "__main__":
+    start_server(HOST, PORT)
