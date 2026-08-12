10181401239d3d57c4e52a60a45ac8baa743bf40
svmod3 <severemod3@gmail.com>
2026-08-12 23:29:54 +0200

    feat(scanner): add SSL connection handler and password sender for bandit16

--- networking/scanner/nmap-scanner/main.py
+++ networking/scanner/nmap-scanner/main.py
@@ -31,13 +31,44 @@ def network_scanner(host: str, ports: str):
     return open_ports
 
 
+def ssl_connection(host: str, port: int, password: str):
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        with context.wrap_socket(sock, server_hostname=host) as ssock:
+            ssock.connect((host, port))
+            ssock.sendall(f"{password}\n".encode("utf-8"))
+
+            response = ssock.recv(4096)
+            return response.decode("utf-8", errors="ignore")
+
+
+
+
 def main(host, ports):
+
+    with open("/etc/bandit_pass/bandit16", 'r') as f:
+        password = f.read().strip()
+
     open_ports = network_scanner(host, ports)
-    print(open_ports)
+
+
     for port in open_ports:
-        num, status, tech = port.split(maxsplit=2)
+        raw_port, status, tech = port.split(maxsplit=2)
+
         if 'ssl' in tech:
-            print(f"Port {tech}: {num}")
+            port_num = int(raw_port.split("/")[0])
+            logging.info(f"\n [+] Próba połączenia z {tech} na porcie {port_num}")
+
+            try:
+                resp = ssl_connection(host, port_num, password)
+                logging.info(f"Odpowiedz z portu {port_num}:\n{resp}")
+            except Exception as e:
+                logging.info(f"Błąd na porcie {num_port}: {e}")
+
 
 
 if __name__ == "__main__":
